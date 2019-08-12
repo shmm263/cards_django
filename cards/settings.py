96546bd47a064bfa5f95aa19435353e31943538a
shmm2 <shmm263@gmail.com>
2019-08-13 02:30:31 +0300

    Added files and changes required for deployment to heroku

--- cards/settings.py
+++ cards/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '7yid@7%nqskux*7&slxqorqz1v#cc*
 #DEBUG = True
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['cards-medical.herokuapp.com','127.0.0.1']
 
 
 # Application definition
